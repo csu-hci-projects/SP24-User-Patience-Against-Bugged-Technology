07b5af7a71ac5035a3e06b3e0a3f91e2c15e77cb
Ri Nabbefeld <62114842+rinabbed@users.noreply.github.com>
2024-03-21 19:56:09 -0600

    Update and rename main.php to index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     </head>
     <body>
     <ul>
-            <li><a href="main.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="survey.php">Take The Survey</a></li>
         </ul>
         <h1><center>Survey Homepage: WIP</center><h1>
