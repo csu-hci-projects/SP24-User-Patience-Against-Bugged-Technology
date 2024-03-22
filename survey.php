c8167e413c5c55a985be57b8f9d87197df0f0294
Ri Nabbefeld <62114842+rinabbed@users.noreply.github.com>
2024-03-21 19:38:31 -0600

    survey add

---
+++ survey.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang ="en" >
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title> Survey </title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Survey Page for Cs464 Experiment">
+        <meta name="keywords" content="cs464, CSU, CSS, HTML">
+        <meta name="author" content="Ri Nabbefeld">
+    </head>
+    <body>
+    <ul>
+            <li><a href="main.php">Home</a></li>
+            <li><a href="survey.php">Take The Survey</a></li>
+        </ul>
+        I don't have anything for ya yet...
+    </body><br>
+    <footer>
+              footer
+    </footer>
+</html>
