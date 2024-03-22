46ccdb7531e63580856faece280b2185828179de
Ri Nabbefeld <62114842+rinabbed@users.noreply.github.com>
2024-03-21 19:04:25 -0600

    These are the file setups for doing this one way

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang ="en" >
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title> Survey Homepage </title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Homepage for Cs464 Experiment">
+        <meta name="keywords" content="Homepage, Ri Nabbefeld, cs464, CSU, CSS, HTML">
+        <meta name="author" content="Ri Nabbefeld">
+    </head>
+    <body>
+        <h1><center>Work In Progress</center><h1>
+        text here
+    </body><br>
+    <footer>
+              footer
+    </footer>
+</html>
