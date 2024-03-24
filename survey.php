4023087e571a043ec9e57d74144fbf0cd96e8ccf
maricodes <maria.lake.cs@gmail.com>
2024-03-23 20:26:44 -0600

    not sure if this works just yet but im manifesting

--- survey.php
+++ survey.php
@@ -13,8 +13,12 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="survey.php">Take The Survey</a></li>
         </ul>
-        I don't have anything for ya yet...
+        Instructions:
+        Please complete the following survey to the best of your abilities. Feel free to quit the survey at any time for any reason- upon quitting, you will be directed to a short "experience" survey to report any issues you may have had with the survey.
+
+        Thank you for your participation!
     </body><br>
+    <input type="button" class="button" value="Start Survey" onclick="window.location.href='demographics.php'" />
     <footer>
               footer
     </footer>
