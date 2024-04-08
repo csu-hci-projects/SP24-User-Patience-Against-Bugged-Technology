4d49d9be823095fde47df297c87d388e9753705d
maricodes <maria.lake.cs@gmail.com>
2024-04-07 22:08:50 -0600

    36-39

--- surveyPage4.php
+++ surveyPage4.php
@@ -10,35 +10,68 @@
     <body>
         <br><br><br><br>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 36</center><h4>
+            <h5><center>Are you introverted or extroverted?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q1A1" name="Q1"/>
-                    <label for="Q1A1">Answer</label>
+                    <input type="radio" value="Introverted" id="Q1A1" name="Q1"/>
+                    <label for="Q1A1">Introverted</label>
+                </div>
+                <div>
+                    <input type="radio" value="Extroverted" id="Q1A2" name="Q1"/>
+                    <label for="Q1A2">Extroverted</label>
+                </div>
+                <div>
+                    <label for="Q1A3">Ambiverted</label>
+                    <input type="radio" value="" id="Q1A3" name="Q1"/>
+                    <label for="Q1A3">Ambiverted</label>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question</center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 37</center><h4>
+            <h5><center>Would you consider yourself a patient person?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q2A1" name="Q2"/>
-                    <label for="Q2A1">Answer</label>
+                    <input type="radio" value="Yes" id="Q2A1" name="Q2"/>
+                    <label for="Q2A1">Yes</label>
+                </div>
+                <div>
+                    <input type="radio" value="No" id="Q2A2" name="Q2"/>
+                    <label for="Q2A2">No</label>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 38</center><h4>
+            <h5><center>Do you value material wealth or emotional wealth more?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q3A1" name="Q3"/>
-                    <label for="Q3A1">Answer</label>
+                    <input type="radio" value="Material" id="Q3A1" name="Q3"/>
+                    <label for="Q3A1">Material Wealth</label>
+                </div>
+                <div>
+                    <input type="radio" value="Emotional" id="Q3A2" name="Q3"/>
+                    <label for="Q3A2">Emotional Wealth</label>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question</center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 39</center><h4>
+            <h5><center>Which of the folowing would you say you are most scared of?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q4A1" name="Q4"/>
-                    <label for="Q4A1">Answer</label>
+                    <input type="radio" value="Heights" id="Q4A1" name="Q4"/>
+                    <label for="Q4A1">Heights</label>
+                </div>
+                <div>
+                    <input type="radio" value="Confined Spaces" id="Q4A2" name="Q4"/>
+                    <label for="Q4A2">Confined Spaces</label>
+                </div>
+                <div>
+                    <input type="radio" value="The Dark" id="Q4A3" name="Q4"/>
+                    <label for="Q4A3">The Dark</label>
+                </div>
+                <div>
+                    <input type="radio" value="Spiders" id="Q4A4" name="Q4"/>
+                    <label for="Q4A4">Spiders</label>
+                </div>
+                <div>
+                    <input type="radio" value="Supernatural" id="Q4A5" name="Q4"/>
+                    <label for="Q4A5">Supernatural</label>
                 </div>
         </div>
         <div class="question-container">
