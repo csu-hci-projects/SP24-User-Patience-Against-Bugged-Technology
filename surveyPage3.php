cd060897cce32e10daaf1e704f0d626840be79f2
Tegan Wall <tegan@colostate.edu>
2024-04-08 18:29:53 -0600

    fixes

--- surveyPage3.php
+++ surveyPage3.php
@@ -13,11 +13,11 @@
             <h4><center>Question 26</center><h4>
             <h5><center>Dogs or cats?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q26A1" name="Q26"/>
+                    <input type="radio" value="Dogs" id="Q26A1" name="Q26"/>
                     <label for="Q26A1">Dogs</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q26A2" name="Q26"/>
+                    <input type="radio" value="Cats" id="Q26A2" name="Q26"/>
                     <label for="Q26A2">Cats</label>
                 </div>
         </div>
@@ -25,15 +25,15 @@
             <h4><center>Question 27</center><h4>
             <h5><center>White, milk, or dark chocolate?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q27A1" name="Q27"/>
+                    <input type="radio" value="White" id="Q27A1" name="Q27"/>
                     <label for="Q27A1">White</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q27A2" name="Q27"/>
+                    <input type="radio" value="Milk" id="Q27A2" name="Q27"/>
                     <label for="Q27A2">Milk</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q27A3" name="Q27"/>
+                    <input type="radio" value="Chocolate" id="Q27A3" name="Q27"/>
                     <label for="Q27A3">Chocolate</label>
                 </div>
         </div>
@@ -41,11 +41,11 @@
             <h4><center>Question 28</center><h4>
             <h5><center>Do you prefer waffles or pancakes?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q28A1" name="Q28"/>
+                    <input type="radio" value="Waffles" id="Q28A1" name="Q28"/>
                     <label for="Q28A1">Waffles</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q28A2" name="Q28"/>
+                    <input type="radio" value="Pancakes" id="Q28A2" name="Q28"/>
                     <label for="Q28A2">Pancakes</label>
                 </div>
         </div>
@@ -53,11 +53,11 @@
             <h4><center>Question 29</center><h4>
             <h5><center>Would you rather have a sweet or savory breakfast?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q29A1" name="Q29"/>
+                    <input type="radio" value="Sweet" id="Q29A1" name="Q29"/>
                     <label for="Q29A1">Sweet</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q29A2" name="Q29"/>
+                    <input type="radio" value="Savory" id="Q29A2" name="Q29"/>
                     <label for="Q29A2">Savory</label>
                 </div>
         </div>
@@ -65,11 +65,11 @@
             <h4><center>Question 30</center><h4>
             <h5><center>Do you prefer to wear more masculine or feminine clothes?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q30A1" name="Q30"/>
+                    <input type="radio" value="Masculine" id="Q30A1" name="Q30"/>
                     <label for="Q30A1">Masculine</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q30A2" name="Q30"/>
+                    <input type="radio" value="Feminine" id="Q30A2" name="Q30"/>
                     <label for="Q30A2">Feminine</label>
                 </div>
         </div>
@@ -77,11 +77,11 @@
             <h4><center>Question 31</center><h4>
             <h5><center>Do you like morning or night showers better?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q31A1" name="Q31"/>
+                    <input type="radio" value="Morning" id="Q31A1" name="Q31"/>
                     <label for="Q31A1">Morning</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q31A2" name="Q31"/>
+                    <input type="radio" value="Night" id="Q31A2" name="Q31"/>
                     <label for="Q31A2">Night</label>
                 </div>
         </div>
@@ -89,11 +89,11 @@
             <h4><center>Question 32</center><h4>
             <h5><center>Vampires or werewolves?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q32A1" name="Q32"/>
+                    <input type="radio" value="Vampires" id="Q32A1" name="Q32"/>
                     <label for="Q32A1">Vampires</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q32A2" name="Q32"/>
+                    <input type="radio" value="Werewolves" id="Q32A2" name="Q32"/>
                     <label for="Q32A2">Werewolves</label>
                 </div>
         </div>
@@ -101,11 +101,11 @@
             <h4><center>Question 33</center><h4>
             <h5><center>Burritos or tacos?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q33A1" name="Q33"/>
+                    <input type="radio" value="Burritos" id="Q33A1" name="Q33"/>
                     <label for="Q33A1">Burritos</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q33A2" name="Q33"/>
+                    <input type="radio" value="Tacos" id="Q33A2" name="Q33"/>
                     <label for="Q33A2">Tacos</label>
                 </div>
         </div>
@@ -113,11 +113,11 @@
             <h4><center>Question 34</center><h4>
             <h5><center>Do you prefer dawn or dusk?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q34A1" name="Q34"/>
+                    <input type="radio" value="Dusk" id="Q34A1" name="Q34"/>
                     <label for="Q34A1">Dusk</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q34A2" name="Q34"/>
+                    <input type="radio" value="Dawn" id="Q34A2" name="Q34"/>
                     <label for="Q34A2">Dawn</label>
                 </div>
         </div>
@@ -125,11 +125,11 @@
             <h4><center>Question 35</center><h4>
             <h5><center>Do you sleep on the left or the right side of the bed?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q35A1" name="Q35"/>
+                    <input type="radio" value="Left" id="Q35A1" name="Q35"/>
                     <label for="Q35A1">Left</label>
                 </div>
                 <div>
-                    <input type="radio" value="" id="Q35A2" name="Q35"/>
+                    <input type="radio" value="Right" id="Q35A2" name="Q35"/>
                     <label for="Q35A2">Right</label>
                 </div>
         </div>
