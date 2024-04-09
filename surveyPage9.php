361a5f1b7c0df6755f9f56b34b5342b3c37b8fd6
Tegan Wall <tegan@colostate.edu>
2024-04-08 21:47:35 -0600

    questions

--- surveyPage9.php
+++ surveyPage9.php
@@ -10,84 +10,73 @@
     <body>
         <br><br><br><br>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 86</center><h4>
+            <h5><center>What is your favorite ice cream flavor?</center><h5>
+            <div>
+                <input type="text" id="Q86A1" name="Q86"/>
+            </div>
+        </div>
+        <div class="question-container">
+            <h4><center>Question 87</center><h4>
+            <h5><center>What is your favorite animal?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q1A1" name="Q1"/>
-                    <label for="Q1A1">Answer</label>
+                    <input type="text" id="Q87A1" name="Q87"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question</center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 88</center><h4>
+            <h5><center>What is your favorite type of pizza?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q2A1" name="Q2"/>
-                    <label for="Q2A1">Answer</label>
+                    <input type="text" id="Q88A1" name="Q88"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 89</center><h4>
+            <h5><center>What is your favorite subject to study?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q3A1" name="Q3"/>
-                    <label for="Q3A1">Answer</label>
+                    <input type="text" id="Q89A1" name="Q89"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question</center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 90</center><h4>
+            <h5><center>What is your favorite movie?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q4A1" name="Q4"/>
-                    <label for="Q4A1">Answer</label>
+                    <input type="text" id="Q90A1" name="Q90"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 91</center><h4>
+            <h5><center>What is your favorite TV show?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q5A1" name="Q5"/>
-                    <label for="Q5A1">Answert</label>
+                    <input type="text" id="Q91A1" name="Q91"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 92</center><h4>
+            <h5><center>What is your favorite song?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q6A1" name="Q6"/>
-                    <label for="Q6A1">Answer</label>
+                    <input type="text" id="Q92A1" name="Q92"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 93</center><h4>
+            <h5><center>What is your favorite item of clothing?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q7A1" name="Q7"/>
-                    <label for="Q7A1">Answer</label>
+                    <input type="text" id="Q93A1" name="Q93"/>
                 </div>
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 94</center><h4>
+            <h5><center>What is your favorite month oof the year?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q8A1" name="Q8"/>
-                    <label for="Q8A1">Answer</label>
-                </div>
+                    <input type="text" id="Q94A1" name="Q94"/>
+                </div> 
         </div>
         <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
+            <h4><center>Question 95</center><h4>
+            <h5><center>What is your favorite beach activity?</center><h5>
                 <div>
-                    <input type="radio" value="" id="Q9A1" name="Q9"/>
-                    <label for="Q9A1">Answer</label>
-                </div>
-                
-        </div>
-        <div class="question-container">
-            <h4><center>Question </center><h4>
-            <h5><center>?</center><h5>
-                <div>
-                    <input type="radio" value="" id="Q3A1" name="Q10"/>
-                    <label for="Q3A1">Answer</label>
+                    <input type="text" id="Q95A1" name="Q95"/>
                 </div>
         </div>
         <a href="index.php" class="button quit-button">Quit Survey</a>
