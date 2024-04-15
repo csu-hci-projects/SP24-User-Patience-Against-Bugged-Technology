df08b8018de3e2c21d9b83f3c03d8e43bb98093f
Tegan Wall <tegan@colostate.edu>
2024-04-14 20:01:44 -0600

    Question 25

--- surveyPage2.php
+++ surveyPage2.php
@@ -205,7 +205,7 @@
         </div>
         <div class="question-container">
             <h4><center>Question 25</center><h4>
-            <h5><center>What is your favorite low-budget meal?</center><h5>
+            <h5 class="hover-heading"><center><span>What is your favorite low-budget meal?</span></center><h5>
                 <div>
                     <input type="radio" value="Mac and Cheese" id="Q3A1" name="Q10"/>
                     <label for="Q3A1">Mac and Cheese</label>
