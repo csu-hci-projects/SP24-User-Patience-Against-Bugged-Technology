41b261b50581c49dc13ab2abc2c35829fbb050bc
rinabbed <rinabbed@gmail.com>
2024-04-18 12:13:01 -0600

    formatting

--- index.php
+++ index.php
@@ -23,15 +23,16 @@
             <br><br>
             This survey will collect data on your age rage, gender, and tech experience. You will also be timed as you take the survey, but there is no time limit.
             <br><br><center>
-
+            
+            <h3><b> You can quit the survey at any time, for any reason. Please do not take the survey multiple times.</b></h3>
             <br>
             
         </h5>
     </body><br>
-    <footer> <center>
+    <footer> <center><h6>
         Contact Us: <br>
         Github: <a href="https://github.com/csu-hci-projects/SP24-User-Patience-Against-Bugged-Technology">Our Repository</a><br>
         Email: ri1223@colostate.edu <br>
         <a href="https://www.cs.colostate.edu/~cs464/#/">Course Website for CS464</a>
-    <center> </footer>
+    <center> </footer></h6>
 </html>
